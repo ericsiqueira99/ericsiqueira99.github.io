c7620ad1fc84fe944734aec558d724a03ed82820
eric <ericsiqueira99@gmail.com>
2024-01-12 20:12:30 +0100

    added template and small updates

---
+++ src/js/additions.js
@@ -0,0 +1,23 @@
+// define constants here
+const PAGE_TITLE = "Welcome to my website."
+const PAGE_DESCRIPTION = "Here you can get an overview of my academic and professional life."
+
+
+// define functions here
+document.getElementById("pageTitle").innerHTML = PAGE_TITLE;
+document.getElementById("pageDescription").innerHTML = PAGE_DESCRIPTION;
+
+
+/*
+TODO
+
+HEADER:
+- add link to download cv
+
+SECTIONS:
+- search for expanding div for each thing, like this: [------- v]
+
+FOOTER:
+- change all links to my accounts
+- add message
+*/
